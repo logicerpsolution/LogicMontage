e19a8ada1bf14060d6d7508a7974759ebd3e6782
unknown <logicerpsolutions1990@gmail.com>
2015-12-29 17:03:43 +0530

    SPA

---
+++ ui/components/signin.reel/signin.js
@@ -0,0 +1,18 @@
+/**
+ * @module ui/signin.reel
+ */
+var Montage = require("montage/core/core").Montage,
+    Component = require("montage/ui/component").Component;
+
+/**
+ * @class Signin
+ * @extends Component
+ */
+// exports.Signin = Component.specialize(/** @lends Signin# */ {
+    // constructor: {
+        // value: function Signin() {
+            // this.super();
+        // }
+    // }
+// });
+exports.Signin=Montage.create(Component);
---
+++ ui/components/signup.reel/signup.js
@@ -0,0 +1,18 @@
+/**
+ * @module ui/signup.reel
+ */
+ var Montage = require("montage/core/core").Montage,
+     Component = require("montage/ui/component").Component;
+
+/**
+ * @class Signup
+ * @extends Component
+ */
+// exports.Signup = Component.specialize(/** @lends Signup# */ {
+    // constructor: {
+        // value: function Signup() {
+            // this.super();
+        // }
+    // }
+// });
+exports.Signup=Montage.create(Component);
